472dd928f95c3835659b039c4c28ad6e3cd1432f
Navid Ghavami <navid_ghavami@outlook.com>
2024-11-24 20:26:44 +0330

    User List page has been Created

--- Resume.Domain/Dtos/User/FilterUserDto.cs
+++ Resume.Domain/Dtos/User/FilterUserDto.cs
@@ -6,21 +6,14 @@ namespace Resume.Domain.Dtos.User
     {
         #region Properties
 
-        [Display(Name = "نام و نام خانوادگی")]
-        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public long Id { get; set; }
         public string Fullname { get; set; }
-
-        [Display(Name = "موبایل")]
-        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-        [MaxLength(11, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
         public string Mobile { get; set; }
-
-        [Display(Name = " بلاک شده / نشده")]
+        public string Email { get; set; }
         public bool IsBlock { get; set; }
-
-        [Display(Name = " تصویر آواتار")]
         public string? Avatar { get; set; }
+        public string CreateDate { get; set; }
+
 
         #endregion
     }
---
+++ ServiceHost/Areas/Administration/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Application.Services.Interface.User;
+
+namespace ServiceHost.Areas.Administration.Controllers
+{
+    public class UserController : AdminBaseController
+    {
+        #region Fields
+
+        private readonly IUserService _userService;
+
+
+
+        #endregion
+
+        #region Constructor
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        #endregion
+
+        #region Actions
+
+        #region Users List
+
+        [HttpGet("users-list")]
+        public async Task<IActionResult> UserList()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Create User
+
+
+
+        #endregion
+
+        #region Edit User
+
+        
+
+        #endregion
+
+
+        #endregion
+    }
+}
